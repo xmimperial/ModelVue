4d85a2c9d063ea07461f799d265f2b43ba531ca8
Xm Imperial <xmimperial@gmail.com>
2026-04-15 12:27:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-901727832-9c5a4",
+  "appId": "1:330246075466:web:88b61967b308c998be22b3",
+  "apiKey": "AIzaSyBnNWAQ83y3htVivMHEEIkVoge5K3P2WvI",
+  "authDomain": "studio-901727832-9c5a4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "330246075466"
+};
